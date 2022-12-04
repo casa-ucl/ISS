c0b96b1332fedf024c063f1e3c004979ff1e796a
Patrick Whyte <patrickwhyte@Patricks-MacBook-Pro.local>
2022-12-04 12:41:37 +0000

    Arduino NodeMCU code to connect to API using HTTPS protocol

--- ISS_tracker/ISS_tracker.ino
+++ ISS_tracker/ISS_tracker.ino
@@ -1,77 +1,114 @@
+/*
+ * CASA0019 Group Project - ISS Tracker
+ * Using:
+ * HTTPS Secured Client GET Request
+ * Copyright (c) 2019, circuits4you.com
+ * All rights reserved.
+ * https://circuits4you.com 
+ */
+
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h> 
+#include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
-#include <ArduinoJson.h>
 
-//Provide WiFi credentials
-const char* ssid = "WiFi_Name";
-const char* password = "WiFi_Password";
-//String for storing server response
-String response = "";
-//JSON document
-DynamicJsonDocument doc(2048);
+/* Set these to your desired credentials. */
+const char *ssid = "WIFI_NAME";  //ENTER YOUR WIFI SETTINGS
+const char *password = "WIFI_PASSWORD";
 
-//Our api variables
+//Web/Server address to read/write from 
+const char *host = "api.wheretheiss.at";
+const int httpsPort = 443;  //HTTPS= 443 and HTTP = 80
+
+//SHA1 finger print of certificate use web browser to view and copy
+const char fingerprint[] PROGMEM = "D5 28 75 E9 DB C8 8C 13 7A 9E 2B 04 B0 09 C3 D8 DE 7B 6E FB";
+
+//API variables
 int latitude;
 int longitude;
 String visibility;
 
 void setup() {
+  delay(1000);
   Serial.begin(115200);
-  //Initiate WiFi connection
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
+  delay(1000);
+  WiFi.mode(WIFI_STA);        //Only Station No AP, This line hides the viewing of ESP as wifi hotspot
+  
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
   Serial.println("");
 
+  Serial.print("Connecting");
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.print("WiFi connected with IP: ");
-  Serial.println(WiFi.localIP());
+
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
 }
 
 void loop() {
-  //Initiate HTTP client
-  HTTPClient http;
-  //Initiate WiFi client
-  WiFiClient client;
-  //The API URL
-  String request = "api.wheretheiss.at/v1/satellites/25544";
-  //Start the request
-  http.begin(client, request);
-  //Use HTTP GET request
-  http.GET();
-  //Response from server
-  response = http.getString();
-  //Serial.print(response); //use for debugging
+  WiFiClientSecure httpsClient;    //Declare object of class WiFiClient
 
-  //Parse JSON, read error if any
-  DeserializationError error = deserializeJson(doc, response);
-  if(error) {
-     Serial.print(F("deserializeJson() failed: "));
-     Serial.println(error.f_str());
-     delay(5000);
-     return;
+  Serial.println(host);
+
+  Serial.printf("Using fingerprint '%s'\n", fingerprint);
+  httpsClient.setFingerprint(fingerprint);
+  httpsClient.setTimeout(15000); // 15 Seconds
+  delay(1000);
+  
+  Serial.print("HTTPS Connecting");
+  int r=0; //retry counter
+  while((!httpsClient.connect(host, httpsPort)) && (r < 30)){
+      delay(100);
+      Serial.print(".");
+      r++;
+  }
+  if(r==30) {
+    Serial.println("Connection failed");
+  }
+  else {
+    Serial.println("Connected to web");
+  }
+  
+  //Declare holding variables
+  String getData, Link;
+
+  //GET Data
+  Link = "/v1/satellites/25544";
+
+  Serial.print("requesting URL: ");
+  Serial.println(host+Link);
+
+  httpsClient.print(String("GET ") + Link + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +               
+               "Connection: close\r\n\r\n");
+
+  Serial.println("request sent");
+                  
+  while (httpsClient.connected()) {
+    String line = httpsClient.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("headers received");
+      break;
+    }
   }
 
-  Serial.print("Full response: ");
-  Serial.println(response);
- 
-  latitude = doc["latitude"].as<int>();
-  Serial.print("Latitude: ");
-  Serial.println(latitude);
-
-  longitude = doc["longitude"].as<int>();
-  Serial.print("Longitude: ");
-  Serial.println(longitude);
-
-  visibility = doc["visibility"].as<String>();
-  Serial.print("Visibility: ");
-  Serial.println(visibility);
-
-  //Close connection  
-  http.end();
-  //Wait five seconds
-  delay(5000);
+  Serial.println("reply was:");
+  Serial.println("==========");
+  String line;
+  while(httpsClient.available()){        
+    line = httpsClient.readStringUntil('\n');  //Read Line by Line
+    Serial.println(line); //Print response
+  }
+  Serial.println("==========");
+  Serial.println("closing connection");
+    
+  delay(2000);  //GET Data at every 2 seconds
 }
